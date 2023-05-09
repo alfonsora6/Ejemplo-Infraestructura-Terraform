9b7b1465e0d742f19cd879fe15b98938e9bbb178
Alfonso Roldán Amador <98739201+alfonsora6@users.noreply.github.com>
2023-05-09 13:21:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ provider "azurerm" {
 }
 
 # Crear grupo de recursos en caso de que no exista
-resource "azurerm_resource_group" "tfexample" {
+resource "azurerm_resource_group" "az_rg" {
   name     = "my-terraform-rg"
   location = "West Europe"
 }
 
 # Crear Virtual Network
-resource "azurerm_virtual_network" "tfexample" {
+resource "azurerm_virtual_network" "az_vnet" {
   name                = "my-terraform-vnet"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  location            = azurerm_resource_group.az_rg.location
+  resource_group_name = azurerm_resource_group.az_rg.name
   address_space       = ["10.0.0.0/16"]
 
   tags = {
@@ -34,18 +34,18 @@ resource "azurerm_virtual_network" "tfexample" {
 }
 
 # Crear Subnet en la Virtual Network
-resource "azurerm_subnet" "tfexample" {
+resource "azurerm_subnet" "az_subnet" {
   name                 = "my-terraform-subnet"
-  resource_group_name  = azurerm_resource_group.tfexample.name
-  virtual_network_name = azurerm_virtual_network.tfexample.name
+  resource_group_name  = azurerm_resource_group.az_rg.name
+  virtual_network_name = azurerm_virtual_network.az_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Crear IP Pública
-resource "azurerm_public_ip" "tfexample" {
+resource "azurerm_public_ip" "az_public_ip" {
   name                = "my-terraform-public-ip"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  location            = azurerm_resource_group.az_rg.location
+  resource_group_name = azurerm_resource_group.az_rg.name
   allocation_method   = "Static"
 
   tags = {
@@ -54,10 +54,10 @@ resource "azurerm_public_ip" "tfexample" {
 }
 
 # Crear Network Security Group y reglas
-resource "azurerm_network_security_group" "tfexample" {
+resource "azurerm_network_security_group" "az_net_sec_group" {
   name                = "my-terraform-nsg"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  location            = azurerm_resource_group.az_rg.location
+  resource_group_name = azurerm_resource_group.az_rg.name
 
   security_rule {
     name                       = "HTTP"
@@ -77,16 +77,16 @@ resource "azurerm_network_security_group" "tfexample" {
 }
 
 # Crear Network Interface
-resource "azurerm_network_interface" "tfexample" {
+resource "azurerm_network_interface" "az_net_int" {
   name                = "my-terraform-nic"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  location            = azurerm_resource_group.az_rg.location
+  resource_group_name = azurerm_resource_group.az_rg.name
 
   ip_configuration {
     name                          = "my-terraform-nic-ip-config"
-    subnet_id                     = azurerm_subnet.tfexample.id
+    subnet_id                     = azurerm_subnet.az_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.tfexample.id
+    public_ip_address_id          = azurerm_public_ip.az_public_ip.id
   }
 
   tags = {
@@ -95,17 +95,17 @@ resource "azurerm_network_interface" "tfexample" {
 }
 
 # Crear Network Interface Security Group association
-resource "azurerm_network_interface_security_group_association" "tfexample" {
-  network_interface_id      = azurerm_network_interface.tfexample.id
-  network_security_group_id = azurerm_network_security_group.tfexample.id
+resource "azurerm_network_interface_security_group_association" "az_net_int_sec_group" {
+  network_interface_id      = azurerm_network_interface.az_net_int.id
+  network_security_group_id = azurerm_network_security_group.az_net_sec_group.id
 }
 
 # Crear máquina virtual
-resource "azurerm_linux_virtual_machine" "tfexample" {
+resource "azurerm_linux_virtual_machine" "az_linux_vm" {
   name                            = "my-terraform-vm"
-  location                        = azurerm_resource_group.tfexample.location
-  resource_group_name             = azurerm_resource_group.tfexample.name
-  network_interface_ids           = [azurerm_network_interface.tfexample.id]
+  location                        = azurerm_resource_group.az_rg.location
+  resource_group_name             = azurerm_resource_group.az_rg.name
+  network_interface_ids           = [azurerm_network_interface.az_net_int.id]
   size                            = "Standard_DS1_v2"
   computer_name                   = "myvm"
   admin_username                  = "azureuser"
@@ -131,9 +131,9 @@ resource "azurerm_linux_virtual_machine" "tfexample" {
 }
 
 # Configurar la ejecución de tareas automáticamente al iniciar la MV.
-resource "azurerm_virtual_machine_extension" "tfexample" {
+resource "azurerm_virtual_machine_extension" "az_vm_extension" {
   name                 = "hostname"
-  virtual_machine_id   = azurerm_linux_virtual_machine.tfexample.id
+  virtual_machine_id   = azurerm_linux_virtual_machine.az_linux_vm.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.1"
@@ -150,12 +150,12 @@ resource "azurerm_virtual_machine_extension" "tfexample" {
 }
 
 # Fuente de datos para acceder a las propiedades de una dirección IP pública de Azure existente.
-data "azurerm_public_ip" "tfexample" {
-  name                = azurerm_public_ip.tfexample.name
-  resource_group_name = azurerm_linux_virtual_machine.tfexample.resource_group_name
+data "azurerm_public_ip" "az_public_ip" {
+  name                = azurerm_public_ip.az_public_ip.name
+  resource_group_name = azurerm_linux_virtual_machine.az_linux_vm.resource_group_name
 }
 
 # Variable de salida: Dirección de IP pública
 output "public_ip" {
-  value = data.azurerm_public_ip.tfexample.ip_address
+  value = data.azurerm_public_ip.az_public_ip.ip_address
 }
